5a52fc8bd6c99fc21d08eceaea0b4af5de2ef3a7
AldoQuintal <aldo.quintal@gasomarshal.mx>
2024-03-13 09:46:53 -0600

    .sh

---
+++ team_modbus.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+sudo su && cd /home/pi/Desktop/Modbus_Team && . venv/bin/activate && python3 tanqueTeam.py
+
+
+
+
+# Crontab */20 * * * * /home/gasomarshal/desarrollo/gm.cambiopreciolocal-central/cambioprecio.sh
