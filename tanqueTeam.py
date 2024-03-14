9280dd807e21b341341388cc6e089d532e9c0d2c
AldoQuintal <aldo.quintal@gasomarshal.mx>
2024-03-14 13:06:37 -0600

    vol_ant

--- tanqueTeam.py
+++ tanqueTeam.py
@@ -328,11 +328,12 @@ def procesa_entregas(tank_id, volumen, volumen_ct, temperatura):
 
         if vol_dif > 50 and ini_entrega[0] == 'False':
             print('### Empieza a registrar una entega ###')
+            print(f'Vol_ant: {vol_ant}')
             vol_ref = vol_ant[2]
             now = datetime.now()
             fecha = now.strftime("%Y/%m/%d %H:%M:%S")
             # Valores que vamos a usar de referencia para compararlos cuando finalize la descarga
-            query = f"""UPDATE public."Tanques_tanques" set inicia_entrega = 'True', vol_ref = \'{"{:.2f}".format(float(vol_ref))}\', fecha_ref = \'{fecha}\', vol_ct_ref = \'{"{:.2f}".format(volumen_ct)}\' WHERE vr_tanque = \'{vol_act[0]}\'"""
+            query = f"""UPDATE public."Tanques_tanques" set inicia_entrega = 'True', vol_ref = \'{"{:.2f}".format(float(vol_ref))}\', fecha_ref = \'{fecha}\', vol_ct_ref = \'{"{:.2f}".format(volumen_ct)}\' ,WHERE vr_tanque = \'{vol_act[0]}\'"""
             cur.execute(query)
             conn.commit()
             print(f'Volumen referencia para entrega: {vol_ref}')
